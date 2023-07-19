93fa49aec2f5f4bdeadee440c338c5ed636c9db9
DionyshsTetradhs <dennis.tetradis@hotmail.com>
2023-07-19 21:49:04 +0300

    ..

--- components/search.tsx
+++ components/search.tsx
@@ -4,12 +4,14 @@ import { BiSearchAlt } from 'react-icons/bi';
 export default function Search() {
 
   const [visibility, setVisibility] = useState(true);
+  const [input, setInput] = useState("");
 
-  // const handleSubmit(e: React.SyntheticEvent){
-  //   e.preventDefault();
-  //   console.log(e);
-  //   
-  // }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setInput("");
+    setVisibility(!visibility);
+    
+  }
   
   const handleVisibility = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -32,9 +34,9 @@ export default function Search() {
       {visibility? <><div className="absolute container top-3 mx-0 min-w-full flex flex-col items-center"><button className="absolute zindex-[-10] bg-blue-200 hover:bg-blue-300 text-blue-800 hover:text-white rounded-full p-4 absolute animate-pulse" onClick={handleVisibility}><BiSearchAlt /></button> </div></>:
 
           <div className="absolute container top-4 mx-0 min-w-full flex flex-col items-center">
-      <form className="absolute container top-1 mx-0 min-w-full flex flex-col items-center">
+      <form onSubmit={handleSubmit} className="absolute container top-1 mx-0 min-w-full flex flex-col items-center">
             
-        <input className="absolute animate-appear text-center outline-none bg-gray-100 border-gray-300 focus:outline-none rounded-md w-80 h-12 text-lg" autoFocus id="searchInput" ref={inputRef} onBlur={handleBlur} type="text" placeholder="Search"></input>
+        <input className="absolute animate-appear text-center outline-none bg-gray-100 border-gray-300 focus:outline-none rounded-md w-80 h-12 text-lg" autoFocus id="searchInput" ref={inputRef} onBlur={handleBlur} value={input} onChange={(e)=>{setInput(e.target.value)}} type="text" placeholder="Search"></input>
         <button type="submit"></button>
         
       </form>
