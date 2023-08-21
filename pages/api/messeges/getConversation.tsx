550bc9a822ce7b86876a30a5004d08327fc9ecf4
DionyshsTetradhs <dennis.tetradis@hotmail.com>
2023-08-21 05:02:07 +0300

    chat_fixed

--- pages/api/messeges/getConversation.tsx
+++ pages/api/messeges/getConversation.tsx
@@ -1,33 +1,44 @@
 import prisma from "./../../../lib/prisma.js";
 import { Auth } from "./../../../lib/auth";
 import { Cleanup } from "../../../lib/utils";
-import { Message, Messag } from "../../../components/chat/types";
-import { NextApiRequest, NextApiResponse } from 'next';
+import { Messag, Message } from "../../../components/chat/types";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   if (req.method == "POST") {
     try {
       const authed = await Auth([req.headers.key, req.headers.userid]);
       if (authed) {
         const id = req?.headers.userid;
         const { receiverID } = req.body;
-        const data:Messag[] = await prisma.Messages.findMany({
+        const data: Messag[] = await prisma.Messages.findMany({
           where: {
-            sender_id: id,
-            receiver_id: receiverID,
+            OR: [
+              {
+                sender_id: id,
+                receiver_id: receiverID,
+              },
+              {
+                sender_id: receiverID,
+                receiver_id: id,
+              },
+            ],
           },
           orderBy: {
             createdAt: "desc",
           },
         });
         let username = await prisma.User.findUnique({
-          where:{
-            id:receiverID
-          }
-        })
+          where: {
+            id: receiverID,
+          },
+        });
         username = username.username;
-        const clean_data:Message[] = Cleanup(data, id);
-        res.status(200).send({clean_data, username});
+        const clean_data: Message[] = Cleanup(data, id);
+        res.status(200).send({ clean_data, username });
       } else {
         res.status(401).send("Not authed!");
       }
